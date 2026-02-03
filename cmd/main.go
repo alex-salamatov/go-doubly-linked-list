97cec505e369e3831c279577a056d93b87da4b92
alex-salamatov <alex.salamatov@gmail.com>
2026-02-03 13:47:52 +0100

    adde RemoveAt and InsertAfter methods

--- cmd/main.go
+++ cmd/main.go
@@ -6,9 +6,57 @@ import (
 	"github.com/alex-salamatov/doubly_linked_list/pkg/datastruct"
 )
 
+func PrintDList(dll *datastruct.DoublyLinkedList) {
+	fmt.Printf("DList: ")
+	for i := 0; i < dll.Len(); i++ {
+		val, _ := dll.At(i)
+		fmt.Printf("%d ", val)
+	}
+	fmt.Printf("\n")
+}
+
 func main() {
 	dll := datastruct.MakeDoublyLinkedList()
-	dll.Append(99)
-	elem, _ := dll.At(0)
-	fmt.Printf("Doubly Linked List length = %d, value at position %d is %d", dll.Len(), 0, elem)
+	dll.Append(0)
+	dll.Append(1)
+	dll.Append(2)
+	dll.Append(3)
+	dll.Append(4)
+	dll.Append(5)
+	dll.Append(6)
+
+	pos0 := 0
+	elem0, _ := dll.At(pos0)
+
+	pos1 := 1
+	elem1, _ := dll.At(pos1)
+
+	pos2 := 2
+	elem2, _ := dll.At(pos2)
+
+	fmt.Printf("Doubly Linked List length = %d, value at position %d is %d\n", dll.Len(), pos0, elem0)
+	fmt.Printf("Doubly Linked List length = %d, value at position %d is %d\n", dll.Len(), pos1, elem1)
+	fmt.Printf("Doubly Linked List length = %d, value at position %d is %d\n", dll.Len(), pos2, elem2)
+
+	dll.RemoveAt(4)
+	dll.RemoveAt(dll.Len() - 1)
+	dll.RemoveAt(0)
+
+	posPL := dll.Len() - 2
+	elemPL, _ := dll.At(posPL)
+
+	posL := dll.Len() - 1
+	elemL, _ := dll.At(posL)
+
+	fmt.Printf("Doubly Linked List length = %d, value at position %d is %d\n", dll.Len(), posPL, elemPL)
+	fmt.Printf("Doubly Linked List length = %d, value at position %d is %d\n", dll.Len(), posL, elemL)
+
+	PrintDList(dll)
+
+	dll.InsertAfter(-1, -1)
+	dll.InsertAfter(0, -2)
+	dll.InsertAfter(dll.Len()-2, -3)
+	dll.InsertAfter(dll.Len()-1, -4)
+
+	PrintDList(dll)
 }
--- pkg/datastruct/doubly-linked-list.go
+++ pkg/datastruct/doubly-linked-list.go
@@ -24,6 +24,53 @@ func MakeDnode(val int) *Dnode {
 	return &Dnode{val, nil, nil}
 }
 
+func (dll *DoublyLinkedList) IsValidIndex(i int) bool {
+	if i < 0 || i >= dll.Len() {
+		return false
+	}
+
+	return true
+}
+
+func (dll *DoublyLinkedList) getNodeAt(pos int) *Dnode {
+	if dll.Len() == 0 {
+		return nil
+	}
+
+	if pos < dll.Len()/2 { //navigate from head to tail
+		n := dll.first
+
+		for i := 0; i < pos; i++ {
+			n = n.next
+		}
+
+		return n
+	} else { //navigate from tail to head
+		n := dll.last
+
+		for i := dll.len - 1; i > pos; i-- {
+			n = n.prev
+		}
+
+		return n
+	}
+}
+
+func (dll *DoublyLinkedList) swap(i, j int) error {
+	if !dll.IsValidIndex(i) || !dll.IsValidIndex(j) {
+		return errors.New("some index is out of range")
+	}
+
+	ni := dll.getNodeAt(i)
+	nj := dll.getNodeAt(j)
+
+	tmp := ni.value
+	ni.value = nj.value
+	nj.value = tmp
+
+	return nil
+}
+
 func (dll *DoublyLinkedList) Len() int {
 	return dll.len
 }
@@ -37,37 +84,14 @@ func (dll *DoublyLinkedList) Append(val int) {
 		dll.len = 1
 	} else {
 		dll.last.next = node
+		node.prev = dll.last
 		dll.last = node
 		dll.len++
 	}
 }
 
-func (dll *DoublyLinkedList) getNodeAt(pos int) *Dnode {
-	if dll.Len() == 0 {
-		return nil
-	}
-
-	if pos < dll.Len()/2 { //travers from head to tail
-		n := dll.first
-
-		for i := 0; i <= pos; i++ {
-			n = n.next
-		}
-
-		return n
-	} else { //travers from tail to head
-		n := dll.last
-
-		for i := dll.len - 1; i > pos; i-- {
-			n = n.prev
-		}
-
-		return n
-	}
-}
-
 func (dll *DoublyLinkedList) At(i int) (int, error) {
-	if i < 0 || i >= dll.Len() {
+	if !dll.IsValidIndex(i) {
 		return 0, errors.New("index is out of range")
 	}
 
@@ -76,4 +100,60 @@ func (dll *DoublyLinkedList) At(i int) (int, error) {
 	}
 
 	return dll.getNodeAt(i).value, nil
+}
+
+func (dll *DoublyLinkedList) RemoveAt(i int) error {
+	if !dll.IsValidIndex(i) {
+		return errors.New("index is out of range")
+	}
+
+	if i == 0 {
+		dll.first.next.prev = nil
+		dll.first = dll.first.next
+		dll.len--
+		return nil
+	}
+
+	if i == dll.Len()-1 {
+		dll.last.prev.next = nil
+		dll.last = dll.last.prev
+		dll.len--
+		return nil
+	}
+
+	node := dll.getNodeAt(i)
+	node.prev.next = node.next
+	node.next.prev = node.prev
+	dll.len--
+	return nil
+}
+
+// i == -1 means insert as front element
+func (dll *DoublyLinkedList) InsertAfter(i, value int) error {
+	if !dll.IsValidIndex(i) && i != -1 {
+		return errors.New("index is out of range")
+	}
+
+	node := MakeDnode(value)
+
+	if (i == -1 && dll.Len() == 0) || (i == dll.Len()-1) {
+		dll.Append(value)
+		return nil
+	}
+
+	if i == -1 { //insert in front of list ...
+		node.next = dll.first
+		node.next.prev = node
+		dll.first = node
+		dll.len++
+		return nil
+	}
+
+	afterNode := dll.getNodeAt(i)
+	node.next = afterNode.next
+	afterNode.next = node
+	node.prev = afterNode
+	node.next.prev = node
+	dll.len++
+	return nil
 }
