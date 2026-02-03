537106607e27659fa83e5a7a9964b5e9e48c9fa3
alex-salamatov <alex.salamatov@gmail.com>
2026-02-03 10:32:07 +0100

    added element access by index

--- cmd/main.go
+++ cmd/main.go
@@ -8,5 +8,7 @@ import (
 
 func main() {
 	dll := datastruct.MakeDoublyLinkedList()
-	fmt.Printf("Doubly Linked List length = %d", dll.Len())
+	dll.Append(99)
+	elem, _ := dll.At(0)
+	fmt.Printf("Doubly Linked List length = %d, value at position %d is %d", dll.Len(), 0, elem)
 }
--- pkg/datastruct/doubly-linked-list.go
+++ pkg/datastruct/doubly-linked-list.go
@@ -1,5 +1,9 @@
 package datastruct
 
+import (
+	"errors"
+)
+
 type Dnode struct {
 	value int
 	next  *Dnode
@@ -22,4 +26,54 @@ func MakeDnode(val int) *Dnode {
 
 func (dll *DoublyLinkedList) Len() int {
 	return dll.len
+}
+
+func (dll *DoublyLinkedList) Append(val int) {
+	node := MakeDnode(val)
+
+	if dll.Len() == 0 {
+		dll.first = node
+		dll.last = node
+		dll.len = 1
+	} else {
+		dll.last.next = node
+		dll.last = node
+		dll.len++
+	}
+}
+
+func (dll *DoublyLinkedList) getNodeAt(pos int) *Dnode {
+	if dll.Len() == 0 {
+		return nil
+	}
+
+	if pos < dll.Len()/2 { //travers from head to tail
+		n := dll.first
+
+		for i := 0; i <= pos; i++ {
+			n = n.next
+		}
+
+		return n
+	} else { //travers from tail to head
+		n := dll.last
+
+		for i := dll.len - 1; i > pos; i-- {
+			n = n.prev
+		}
+
+		return n
+	}
+}
+
+func (dll *DoublyLinkedList) At(i int) (int, error) {
+	if i < 0 || i >= dll.Len() {
+		return 0, errors.New("index is out of range")
+	}
+
+	if dll.Len() == 0 {
+		return 0, errors.New("cannot return an element: the list is empty")
+	}
+
+	return dll.getNodeAt(i).value, nil
 }
